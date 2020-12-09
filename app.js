28f4b1ec6597a3bbb7d3c95494b08105b0e27ea6
Tharindu <tharindugimras@gmail.com>
2020-12-10 04:18:01 +0530

    App created part1 completed

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date= require(__dirname +"/date.js");
+
+const itemList = ["bring food","make food","eat food"];
+const workItems = [];
 
 const app = express();
 
 app.set("view engine", "ejs");
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.get("/", function (req, res) {
-  var today = new Date();
-  var options = { weekday:'long', day:'numeric', month:'long'};
-  var day = today.toLocaleString('en-US',options);
-  res.render("list", { kindOfDay: day });
+    let day = date.getDate();
+    res.render("list", { listTitle: day, newListItems: itemList });
 });
 
+app.post("/", function (req, res) {
+    console.log(req.body);
+    let nextItem = req.body.next_item;
+    if (req.body.button=='Work'){
+        workItems.push(nextItem);
+        res.redirect("/Work");
+    }else{
+        itemList.push(nextItem);
+        res.redirect("/");
+    }
+});
+
+app.get("/work",function(req, res){
+    res.render("list", {listTitle:"Work List" , newListItems: workItems})
+})
+
 app.listen(3000, function () {
-  console.log("Server started at port 3000");
+    console.log("Server started at port 3000");
 });
---
+++ date.js
@@ -0,0 +1,18 @@
+//jshint esversion:6
+exports.getDate = getDate;
+
+function getDate() {
+    const today = new Date();
+    const options = { weekday: "long", day: "numeric", month: "long" };
+    const day = today.toLocaleString("en-US", options);
+    return day;
+}
+
+exports.getDay = getDay;
+
+function getDay() {
+    const today = new Date();
+    const options = { weekday: "long" };
+    const day = today.toLocaleString("en-US", options);
+    return day;
+}
